bc988bc93d8aa09ff75c866e61dd102cc1d84457
Karina1014 <janneth_wi15@hotmail.com>
2024-10-06 11:34:24 -0500

    💻 Realizado con exito

--- src/App.js
+++ src/App.js
@@ -4,7 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <div className="sidebar"></div>
+        <div className="sidebar">
+            <div className="upperSide">
+                <div className="upperSideTop">
+                  <img src="" alt="" className="logo"></img>
+                  <span className="brand">CHAT AMAGUAÑA</span>
+                  
+                  <button className="miBtn"></button>
+                  <img src="" alt="" className="addBtn">Nuevo Chat</img>
+
+                </div>
+              
+            </div>
+            <div className="lowerside">
+              
+              </div>
+        </div>
+        <div className="main">
+              
+        </div>
     </div>
   );
 }
